39e4e16b752b1b0077115ea77da578db6e1a8919
mstypulk <matt.stypulkoski@gmail.com>
2019-10-07 22:47:41 -0400

    Updates

---
+++ precache-manifest.b1612e152f14ccd9ef7c2d3a11b08a9d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9828cdbf694e4f45b326ceb3cfa751",
+    "url": "/static/media/StevensLogo_1024px.6d9828cd.png"
+  },
+  {
+    "revision": "1b7015fb4e602a42c951",
+    "url": "/static/css/main.06c3cfea.chunk.css"
+  },
+  {
+    "revision": "ab54ae5c6367d8dcc88e",
+    "url": "/static/js/1.ab54ae5c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9aa4a8db6af8af88ff90b1226d4e70dc",
+    "url": "/static/media/PictureOfMe_mobile.9aa4a8db.png"
+  },
+  {
+    "revision": "49cb02d5ffe70795c06b7a696e7483f1",
+    "url": "/static/media/AboutMePic_mobile.49cb02d5.png"
+  },
+  {
+    "revision": "bc8958309817c6080e16c36f8309ddd7",
+    "url": "/static/media/HighRollerTitleScreenShot_mobile.bc895830.png"
+  },
+  {
+    "revision": "e0a2631ce8f240cca1473bb022779453",
+    "url": "/static/media/StevensLogo_mobile.e0a2631c.png"
+  },
+  {
+    "revision": "38cdfbf7f76d0a2b83efac490e959810",
+    "url": "/static/media/Unity3DIcon.38cdfbf7.png"
+  },
+  {
+    "revision": "d60136734493e437751bb9cd76e015d3",
+    "url": "/static/media/BlenderIcon.d6013673.png"
+  },
+  {
+    "revision": "6ceaa87fb838204a759bedcbfef8ef02",
+    "url": "/static/media/StevensLogo.6ceaa87f.png"
+  },
+  {
+    "revision": "1b7015fb4e602a42c951",
+    "url": "/static/js/main.1b7015fb.chunk.js"
+  },
+  {
+    "revision": "74287ce47cbdfa622ac13b0edfa302ad",
+    "url": "/static/media/HighRollerTitleScreenShot.74287ce4.PNG"
+  },
+  {
+    "revision": "10a118f0d9c2c1757f92831c145962fb",
+    "url": "/static/media/PictureOfMe_375px.10a118f0.png"
+  },
+  {
+    "revision": "c027ad80b8a532522ff06276a32f83ff",
+    "url": "/static/media/HighRollerTitleScreenShot_1300px.c027ad80.png"
+  },
+  {
+    "revision": "524d720f3f670bd38785447ca9c4b395",
+    "url": "/static/media/BebasNeue Bold.524d720f.otf"
+  },
+  {
+    "revision": "aef9e527700eede098ef4e1a4f3934fe",
+    "url": "/static/media/PictureOfMe_600px.aef9e527.png"
+  },
+  {
+    "revision": "61e8a11f30a63f1caaf625039493e89f",
+    "url": "/static/media/AboutMePic_600px.61e8a11f.png"
+  },
+  {
+    "revision": "255ebd859b0114c85b0cfd76905be4be",
+    "url": "/static/media/AboutMePic_768px.255ebd85.png"
+  },
+  {
+    "revision": "a1da32574b0edc3874b7365fa652e6ae",
+    "url": "/static/media/PictureOfMe_768px.a1da3257.png"
+  },
+  {
+    "revision": "63c3de80b02715b8282074c13ecd21dd",
+    "url": "/static/media/AboutMePic_1024px.63c3de80.png"
+  },
+  {
+    "revision": "6660d0e36b0767c4fce9b63d6d38a7f9",
+    "url": "/static/media/PictureOfMe.6660d0e3.png"
+  },
+  {
+    "revision": "87f6e52a70c1abe939117770d75164a4",
+    "url": "/index.html"
+  }
+];
