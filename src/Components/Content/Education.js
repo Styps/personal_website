b5074c9e6f825d1a1ea664fc167dce37585631b1
Matthew Stypulkoski <Matthew.Stypulkoski@ibm.com>
2019-10-07 20:40:17 -0400

    Change li elements to p to keep consistency

--- src/Components/Content/Education.js
+++ src/Components/Content/Education.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import StevensLogoMobile from './../../images/EducationImages/StevensLogo_mobile.png';
 
 const Education = React.forwardRef((props, ref) =>
@@ -12,10 +12,8 @@ const Education = React.forwardRef((props, ref) =>
             <p>Hoboken, NJ</p>
 
             <h4>Earned Bachelor of Science</h4>
-            <ul>
-                <li>Computer Science</li>
-                <li>Graduated with Honors</li>
-            </ul>
+            <p>Computer Science</p>
+            <p>Graduated with Honors</p>
 
             <p className="education_text">I learned a lot from my time at Stevens. I came into college with no programming knowledge, but I left with an immense skill set and confidence. Stevens allowed me to collaborate with
             other students through both class projects and my time on the Filipino Association of Steven Tech's (FAST) Executive Board. I gained memorable moments, and made friends for life.</p>
